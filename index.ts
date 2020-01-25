0a1842d4fe974040c5c8054d8b78ea58f3426e42
zaru <zarutofu@gmail.com>
2020-01-25 18:44:30 +0900

    temporary implement getUserMedia()

--- index.ts
+++ index.ts
@@ -1,1 +1,24 @@
 console.log('hello world')
+
+function getStream() {
+  const config = {
+    video: {
+      audio: false,
+      facingMode: { exact: 'environment' }
+    }
+  }
+  return navigator.mediaDevices.getUserMedia(config)
+}
+
+async function showStream() {
+  const stream = await getStream()
+  const video = document.getElementById('video') as HTMLVideoElement
+  video.srcObject = stream
+  video.addEventListener('loadedmetadata', () => {
+    video.play()
+  })
+}
+
+window.onload = () => {
+  showStream()
+}
