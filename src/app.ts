62244bdfac6eac874be2d3ff0d06f1f1d7885502
zaru <zarutofu@gmail.com>
2020-01-28 00:20:01 +0900

    draw qr rect

--- src/app.ts
+++ src/app.ts
@@ -1,77 +1,22 @@
 import { Camera } from './camera'
-import jsQR from 'jsqr'
-
-class QrReader {
-  readonly document: HTMLDocument
-  readonly camera: Camera
-  private timerId
-  private interval: number
-  private result: string[]
-  private callback: (result: string[]) => void
-
-  constructor(document: HTMLDocument) {
-    this.document = document
-    this.camera = new Camera(this.document)
-    this.camera.showStream()
-    this.result = []
-  }
-
-  get intervalTime(): number {
-    return this.interval
-  }
-
-  set intervalTime(msec) {
-    this.interval = msec
-  }
-
-  set updateCallback(callback: (result: string[]) => void) {
-    this.callback = callback
-  }
-
-  start(): void {
-    this._parseQR()
-    this.timerId = setInterval(() => {
-      this._parseQR()
-    }, this.intervalTime)
-  }
-
-  stop(): void  {
-    clearInterval(this.timerId)
-  }
-
-  _parseQR(): void  {
-    const data = this.camera.capture()
-    const code = jsQR(data, this.camera.video.videoWidth, this.camera.video.videoHeight)
-    if (code && !this.result.includes(code.data)) {
-      this.result.push(code.data)
-      this.callback(this.result)
-    }
-  }
-}
 
 window.onload = (): void => {
   const resultElm = document.getElementById('result') as HTMLTextAreaElement
-  const videoElm = document.getElementById('video') as HTMLVideoElement
+  const canvasElm = document.getElementById('canvas') as HTMLCanvasElement
 
-  const reader = new QrReader(document)
-  reader.intervalTime = 1000
+  const reader = new Camera(window)
   reader.updateCallback = (result): void => {
     resultElm.value = result.join("\n")
     resultElm.scrollTop = resultElm.scrollHeight
     console.log(result)
-    videoElm.classList.add('fadeoutin')
+    canvasElm.classList.add('fadeoutin')
   }
+  reader.showStream()
 
-  videoElm.addEventListener('animationend', () => {
-    videoElm.classList.remove('fadeoutin')
+  canvasElm.addEventListener('animationend', () => {
+    canvasElm.classList.remove('fadeoutin')
   })
 
-  document.getElementById('start').addEventListener('click', () => {
-    reader.start()
-  })
-  document.getElementById('stop').addEventListener('click', () => {
-    reader.stop()
-  })
   document.getElementById('copy').addEventListener('click', () => {
     resultElm.select()
     document.execCommand('copy')
--- src/camera.ts
+++ src/camera.ts
@@ -1,18 +1,25 @@
+import jsQR from 'jsqr'
+
 export class Camera {
+  readonly window: Window
   readonly document: HTMLDocument
   readonly video: HTMLVideoElement
   readonly canvas: HTMLCanvasElement
+  private ctx: CanvasRenderingContext2D
+  private result: string[]
+  private callback: (result: string[]) => void
 
-  constructor(document: HTMLDocument) {
-    this.document = document
+  constructor(window: Window) {
+    this.window = window
+    this.document = this.window.document
     this.video = this.document.getElementById('video') as HTMLVideoElement
     this.canvas = this.document.getElementById('canvas') as HTMLCanvasElement
+    this.ctx = this.canvas.getContext('2d')
+    this.result = []
   }
 
-  capture(): Uint8ClampedArray {
-    const ctx = this.canvas.getContext('2d')
-    ctx.drawImage(this.video, 0, 0, this.video.videoWidth, this.video.videoHeight)
-    return ctx.getImageData(0, 0, this.video.videoWidth, this.video.videoHeight).data
+  set updateCallback(callback: (result: string[]) => void) {
+    this.callback = callback
   }
 
   async showStream(): Promise<void> {
@@ -20,7 +27,39 @@ export class Camera {
     this.video.addEventListener('loadedmetadata', () => {
       this._adjustCanvasSize()
       this.video.play()
+      this.window.requestAnimationFrame(() => this._syncCanvas())
     })
+  }
+
+  _drawBox(location, color): void {
+    const rect = [
+      [location.topLeftCorner, location.topRightCorner],
+      [location.topRightCorner, location.bottomRightCorner],
+      [location.bottomRightCorner, location.bottomLeftCorner],
+      [location.bottomLeftCorner, location.topLeftCorner]
+    ]
+    rect.forEach(combi => {
+      this.ctx.beginPath()
+      this.ctx.moveTo(combi[0].x, combi[0].y)
+      this.ctx.lineTo(combi[1].x, combi[1].y)
+      this.ctx.lineWidth = 4
+      this.ctx.strokeStyle = color
+      this.ctx.stroke()
+    })
+  }
+
+  _syncCanvas(): void {
+    this.ctx.drawImage(this.video, 0, 0, this.video.videoWidth, this.video.videoHeight)
+    const data = this.ctx.getImageData(0, 0, this.video.videoWidth, this.video.videoHeight).data
+    const code = jsQR(data, this.video.videoWidth, this.video.videoHeight)
+    if (code) {
+      this._drawBox(code.location, '#ff0000')
+       if (!this.result.includes(code.data)) {
+         this.result.push(code.data)
+         this.callback(this.result)
+       }
+    }
+    this.window.requestAnimationFrame(() => this._syncCanvas())
   }
 
   _getStream(): Promise<MediaStream> {
